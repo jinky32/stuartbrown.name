c6113345f2266cf3f33986588362055ba1653299
Stuart Brown <stuartbrown@mcl240297.home.gateway>
2014-01-07 22:00:33 +0000

    pulling in a fuller feed

--- youtube/apitest.php
+++ youtube/apitest.php
@@ -1,8 +1,11 @@
 <?php
+// I need something else in here that will pull all of my playlists from https://gdata.youtube.com/feeds/api/users/jinky32/playlists
+// in feed/entry/<yt:playlistId>.  URL var below can then use that value
 $html ="";
-$url="https://gdata.youtube.com/feeds/api/playlists/PLEtmlR7ubZ2mDq0IEd8z1IqfvnKegSeT9";
+$url="https://gdata.youtube.com/feeds/api/playlists/PLEtmlR7ubZ2nf5OYTqGws_vTKCQYwSj6e";
 $xml=simplexml_load_file($url);
 // print_r($xml);
+$playlist=$xml->title;
 
 for($i=0; $i<10; $i++) {
 
@@ -13,7 +16,7 @@ for($i=0; $i<10; $i++) {
 
 	$html .= "<div><h3>$title</h3>$content<br />$author - $id</div><hr />";
 }
-
+echo "<h1>$playlist</h1>";
 echo $html;
 
 ?>
