815dd47ed4eadb16ad9baca138238c880a820cbb
Stuart Brown <stuartbrown@mcl240297.home.gateway>
2014-01-23 15:45:39 +0000

    photos are now added to the database.  However this has introduced issue #1 where images are not getting deleted from the database when unfavourtied.  LIkely a typo because this was working

--- photography/_/components/php/500pxapi.php
+++ photography/_/components/php/500pxapi.php
@@ -1,16 +1,5 @@
-<?php //namespace Photo\DB;
+<?php namespace Photo\DB;
 require "_/components/php/500pxhardcoded.php";
-// //create as array of the 500px categories and category ID.  Only ID is returned in the API array so the label needs to be matched later
-// $sitecategories = array(
-//     0 => "Uncategorized", 10 => "Abstract", 11 => "Animals", 5 => "Black and White", 1 => "Celebrities", 9 => "City and Architecture",
-//     15 => "Commercial", 16 => "Concert", 20 => "Family", 14 => "Fashion", 2 => "Film", 24 => "Fine Art", 23 => "Food",
-//     3 => "Journalism", 8 => "Landscapes", 12 => "Macro", 18 => "Nature", 4 => "Nude", 7 => "People", 19 => "Performing Arts",
-//     17 => "Sport", 6 => "Still Life", 21 => "Street", 26 => "Transportation", 13 => "Travel", 22 => "Underwater",
-//     27 => "Urban Exploration", 25 => "Wedding"
-//     );
-// //end of 500pc categories array
-
-
 
     $comsumer_key = 'I9CDYnaxrFxLTEvYxTmsDKZQlgStyLNQkmtOKGKb';
     $username = 'jinky32';
@@ -41,38 +30,87 @@ require "_/components/php/500pxhardcoded.php";
         } 
 
 
-
-
-
 $nonunique=array(); //initiate $nonunique array.  This will hold the full list of category IDs from the 500px API.  $categories below will be used to get only unique IDs in order to create the primary navigation.
 
 $photoname=array(); // intitiate $photoname array.  Holds the names of the photographs from the API array
 
 $categories=array(); // intitiate $categories array.  This will be filtered to contain only unique values to drive the primary navigation labels.
 
-foreach ($obj->photos as $photo){ //loop through photos and set values of arrays
-    $categories[]=$photo->category;
-    $nonunique[]=$photo->category;
-    $photoname[]=$photo->name;
+foreach ($obj->photos as $photos_500px){ //loop through photos and set values of arrays
+    $categories[]=$photos_500px->category;
+    $nonunique[]=$photos_500px->category;
+    $photoname[]=$photos_500px->name;
  
     }
     $combined=array_combine($photoname, $nonunique); 
 
 
+//break apart the $combined array and insert the values into the database.  The db then contains all the items 
+//from 500px that i have favourited.
+    foreach ($combined as $key => $value) {
+    if ( $conn ) { //break the array apart and pass value for insert to the query() function in functions.php
+      $combinedquery=query("INSERT INTO images(photo_title, cat_id) 
+        VALUES (:photo_title, :cat_id)
+        ON DUPLICATE KEY UPDATE photo_title = VALUES(photo_title)",
+      array('photo_title'=>$key, 'cat_id'=>$value), //bind the values
+      $conn);
+    } else {
+      print "could not connect to the database";
+    }
+  }
 
-$categories=array_unique($categories); //make categories contian only unique category ID from API in order to create primary nav labels
+//check if there are values in the images table of the dtabase.  this will be used below to test against API values
+if ( $conn ) {
+      $photos_database=query2("SELECT cat_id, photo_title FROM images", 
+      $conn);
+} else {
+      print "could not connect to the database";
+}
+
+//below is the same script as used in 500pxhardcoded.php.  It checks the values from 500px api with the values in the database
+//values from 500px api are the master so if there is a difference then the db is updated with api data
+
+
+if (!empty($photos_database)){  //if there are values in the images table
+
+  $photoarray_database=array(); //initiate $photoarray_database
+  
+  for ($i=0; $i < sizeof($photos_database); $i++) { //loop through the array  and fill $photoarray_database with the cat_id
+    $photoarray_database[]=$photos_database[$i]['cat_id'];
+    $photoarray_database_label[]=$photos_database[$i]['photo_title'];
+  }
+  //print_r($photoarray_database);
+  // print_r($photoarray_database_label);
+
+
+  $photoarray_database_combined=array_combine($photoarray_database_label, $photoarray_database); // combine the values into a new array.  This is used to compare with the images returned from the 500px api with the images from the database.
+  // print_r($photoarray_database_combined);
+  // print_r($categories);
+
+  $photodiff = array_diff($photoarray_database_combined, $combined);  // compare the two arrays and then print the result.  Values of $combined are the master since they come from api.
+
+  // print_r($photodiff);
+  //if there are differences between the two arrays then remove from database.  Additions are delat with thorugh the initial insert which deals with ON DUPLICATE KEY 
+  if($photodiff){ // if there is a difference use that cat_id in a delete statement
+    foreach ($photodiff as $key => $value) { //break apart array to get cat_id value
+      $photodelete=delete("DELETE FROM images where cat_id=$value",$conn);
+    }
+    
+  }
+
+}
+
+
+//then select from the table and use the results to get a unique array_unique() as below.
+
+$photoarray_database=array_unique($photoarray_database); //make photoarray_database contian only unique category ID from API in order to create primary nav labels
 
 $catkeys = array(); //initiate $catkeys array
-foreach($categories as $key => $value){ //loop through $categories
+foreach($photoarray_database as $key => $value){ //loop through $photoarray_database
   $catkeys[$value] = ""; // assign $value (e.g. 9, 12 , 24 etc) as the key and give each an empty value.
 }
 
-//I NEED TO REPLACE $SITECATEGORIES BELOW WITH $CATARRAY_DATABSE FROM 500PXHARDCODED.PHP. HOWEVER I NEED TO MAKE CAT_ID THE KEY IN THE LATTER DB
-//SO THAT I CAN USE ARRAY_INTERSECT_KEY (AS BELOW)
 $intersect = array_intersect_key($catarray_database_combined, $catkeys); //create an array of the items in $catarray_database_combined (from 500pxhardcoded.php) and $catkeys that 
 //are the same.  This is what will go into the primary nav
-
-
-
 
 ?>
