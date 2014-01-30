9200c6c42f3f173ba4974d391c7ef8a6bf97e5d6
Stuart Brown <stuartbrown@mcl240297.open.ac.uk>
2014-01-30 16:15:30 +0000

    fuller api intergration with reports driven from select list

--- dax/_/components/php/dax.php
+++ dax/_/components/php/dax.php
@@ -7,29 +7,79 @@
     $username = 'jinky32';
     $count = 14;
 
-    $ch = curl_init();
-    // curl_setopt($ch, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=editors&page=2&consumer_key=I9CDYnaxrFxLTEvYxTmsDKZQlgStyLNQkmtOKGKb");
+
+
+//start of api call to pull in all report items
+
+    $ch2 = curl_init();
+    // curl_setopt($ch2, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=editors&page=2&consumer_key=I9CDYnaxrFxLTEvYxTmsDKZQlgStyLNQkmtOKGKb");
     
-    curl_setopt($ch, CURLOPT_URL,"https://dax-rest.comscore.eu/v1/reportitems.xml?itemid=38&startdate=20140101&enddate=20140107&site=supersite-external&format=json&client=ou&user=sbrown&password=m4nch3st3r");
-    // curl_setopt($ch, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=user&username={$username}&consumer_key={$comsumer_key}");
-    curl_setopt($ch , CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch2, CURLOPT_URL,"https://dax-rest.comscore.eu/v1/reportitemlist.xml?client=ou&user=sbrown&password=m4nch3st3r&format=json");
+    // curl_setopt($ch2, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=user&username={$username}&consumer_key={$comsumer_key}");
+    curl_setopt($ch2 , CURLOPT_HEADER, 0);
+    curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1); 
 
-   $json = curl_exec($ch);
-   if(curl_errno($ch))
+   $json2 = curl_exec($ch2);
+   if(curl_errno($ch2))
    {
-       // echo 'Curl error: ' . curl_error($ch);
+       // echo 'Curl error: ' . curl_error($ch2);
    }
-   curl_close($ch);
-   $dax = array();
+   curl_close($ch2);
+   $daxreportitems = array();
 
-//decode json response
- if($json){
-      $dax = json_decode($json,true); 
+//decode json2 response
+ if($json2){
+      $daxreportitems = json_decode($json2,true); 
           }
   else {
       print "<p>Currently, No Service Available.</p>";
         } 
+
+//    $rows=array();
+//    for ($i=0; $i <= sizeof($daxreportitems[ri]); $i++) {   
+//    print $daxreportitems[ri][$i]['id']. "<br />";  
+//    print $daxreportitems[ri][$i]['path']. "<br />"; 
+//    //    $rows[$daxreportitems[$i]['id']]=$daxreportitems[$i]['path'];
+//    // print "rows";
+//    // print_r($rows);
+//   }
+// print_r($daxreportitems);
+//end of array call to pull in all report items
+
+
+
+
+
+
+
+
+//start of api call to pull in a particular report
+
+//     $ch = curl_init();
+//     // curl_setopt($ch, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=editors&page=2&consumer_key=I9CDYnaxrFxLTEvYxTmsDKZQlgStyLNQkmtOKGKb");
+    
+//     curl_setopt($ch, CURLOPT_URL,"https://dax-rest.comscore.eu/v1/reportitems.xml?itemid=130&startdate=20140101&enddate=20140107&site=supersite-external&format=json&client=ou&user=sbrown&password=m4nch3st3r");
+//     // curl_setopt($ch, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=user&username={$username}&consumer_key={$comsumer_key}");
+//     curl_setopt($ch , CURLOPT_HEADER, 0);
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+//    $json = curl_exec($ch);
+//    if(curl_errno($ch))
+//    {
+//        // echo 'Curl error: ' . curl_error($ch);
+//    }
+//    curl_close($ch);
+//    $dax = array();
+
+// //decode json response
+//  if($json){
+//       $dax = json_decode($json,true); 
+//           }
+//   else {
+//       print "<p>Currently, No Service Available.</p>";
+//         } 
+
+//end of array call to pull in a particular report
 
 
 // $columns=array();
--- dax/page1.php
+++ dax/page1.php
@@ -19,14 +19,76 @@
     <![endif]-->
 
   </head>
+  <body>
+  <?php
+  if(isset($_POST[report_item])){
+          //$title=$daxreportitems[ri][$i]['path'];
+          print "<h1>ReportItem Id is $_POST[report_item]</h1>";
+        } else {
+          print "<h1>Hello World!</h1>";
+        }
 
+    ?>
+<div class="container">
+<div class="content row">
+ <div class="main col col-lg-2">
+ <form method='post' action=''>
+ <select class='form-control'  name='report_item' id='report_item'>
+
+  <?php
+
+ for ($i=0; $i <= sizeof($daxreportitems[ri]); $i++) {   
+   // print $daxreportitems[ri][$i]['id']. "<br />";  
+   // print $daxreportitems[ri][$i]['path']. "<br />"; 
+  print "<option value='".$daxreportitems[ri][$i]['id']."'>".$daxreportitems[ri][$i]['path']."</option>";
+}
+print "</select>
+           <input type='submit' class='btn btn-default' name='youtube' id='youtube' value='submit'>
+            </form></div>";
+
+  if(isset($_POST[report_item])){
+          // foreach ($_POST['videoselect'] as $skey => $yt_embed_url) {
+          // }
+  //  print_r($_POST);
+$report_id=$_POST[report_item];
+
+        }
+
+
+
+  ?>
+
+ 
+        <div class="main col col-lg-10">
   <div class="table-responsive">
   <table class="table table-hover">
     <thead>
       <tr>
     <?php 
+   $ch = curl_init();
+    // curl_setopt($ch, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=editors&page=2&consumer_key=I9CDYnaxrFxLTEvYxTmsDKZQlgStyLNQkmtOKGKb");
+    
+    curl_setopt($ch, CURLOPT_URL,"https://dax-rest.comscore.eu/v1/reportitems.xml?itemid=$report_id&startdate=20140101&enddate=20140107&site=supersite-external&format=json&client=ou&user=sbrown&password=m4nch3st3r");
+    // curl_setopt($ch, CURLOPT_URL,"https://api.500px.com/v1/photos?feature=user&username={$username}&consumer_key={$comsumer_key}");
+    curl_setopt($ch , CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 
-  
+   $json = curl_exec($ch);
+   if(curl_errno($ch))
+   {
+       // echo 'Curl error: ' . curl_error($ch);
+   }
+   curl_close($ch);
+   $dax = array();
+
+//decode json response
+ if($json){
+      $dax = json_decode($json,true); 
+          }
+  else {
+      print "<p>Currently, No Service Available.</p>";
+        } 
+
   $columns=array();
   for ($i=0; $i <= sizeof($dax[reportitems]['reportitem'][0][columns][column]); $i++) { 
   $columns[]=$dax[reportitems]['reportitem'][0][columns][column][$i];
@@ -35,15 +97,29 @@
     //print "this is key ". $key . " and this is value" . $value ."<br />";
     }
   ?>
-  
-
-      </tr>
+    </tr>
     </thead>
     <tbody>
       <tr>
-        <td></td>
-      </tr>
+  <?php
+  // print $dax[reportitems]['reportitem'][0][rows][r][$i][c];
+   $rows=array();
+   for ($i=0; $i <= sizeof($dax[reportitems]['reportitem'][0][rows][r]); $i++) {     
+      $rows[]=$dax[reportitems]['reportitem'][0][rows][r][$i][c];
+     print "<td>".$rows[$i][0]."</td>";
+     print "<td>".$rows[$i][1]."</td>";
+     print "<td>".$rows[$i][2]."</td>";
+     print "</tr>";
+  }
+  // print_r($rows);
+  ?>
+
+      
     </tbody>
   </table>
   </div>
+  </div>
+</div>
+  </div>
+  </body>
 </html>
