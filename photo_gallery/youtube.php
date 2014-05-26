1ce097b83ea31807b539ca63d8be57c0b68a2515
Stuart Brown <jinky32@gmail.com>
2014-05-26 20:56:39 +0100

    form now remembers the users selections this can be used to make more efficient database query

--- photo_gallery/youtube.php
+++ photo_gallery/youtube.php
@@ -12,37 +12,66 @@ $youtube->create('User','jinky32');
 $playlists = $youtube->youtubeDbPlaylistSelect();
 $image_url = $youtube->youtubeDbPlaylistImageSelect();
 
-//              reset($images);
-// $first_key = key($images);
-//print $first_key;
 
-//print_r($images);
+?>
 
-print '<h1>Select Your Playlists</h1>
-<p>You probably have playlists that don\'t contain relevant videos.  Please select the playlists below that DO contain videos of use</p>';
+<div class="container">
+<h1>Select Your Playlists</h1>
+<p>You probably have playlists that don't contain relevant videos.  Please select the playlists below that DO contain videos of use</p>
+<form method="post" action="">
 
+<?php
 $i=0;
 foreach($playlists as $key => $value) {
               print '<div class="col-sm-6 col-md-4">
               <div class="thumbnail">
-      <img src="'.$image_url[$i].'" alt="...">
+               
+                <label> <input type="checkbox" id="'.$youtube->getUser()->data()->id.' - '.$key.'" 
+        value="'.$youtube->getUser()->data()->id.' - '.$key.'" 
+        name="playlist['.$youtube->getUser()->data()->id.' - '.$key.']"
+        title="Select this Playlist"'?> <?php if(isset($_POST['playlist'][$youtube->getUser()->data()->id.' - '.$key])) print 'class="checked" checked="checked"';?><?php print ' />   Select this Playlist.  You need to submit the form at the bottom of the page. </label>
+      <img src="'.$image_url[$i].'" alt="..." class="img-responsive img-rounded img-centred">
       <div class="caption">
-        <h3>'.$key.'</h3>
+         <h4>'.$key.'</h4>
         <p>some text</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        <a href="#" class="btn btn-default" role="button">Synchronise Playlist</a></p>
       </div>
     </div>
   </div>';
+  //print $_POST['playlist'][$youtube->getUser()->data()->id.' - '.$key];
 $i++;     
           }
+          print '<input type="submit" class="btn btn-default" name="youtube_playlists" id="youtube_playlists" value="submit">';
 
-          ?>  
-  
+         // if(isset($_POST['playlist'][$youtube->getUser()->data()->id.' - '.$key])){print 'class="checkbox" checked';} 
+         // <input name="tape[]"  id="tape[]" type="checkbox" value="NA" <?php if(isset($_POST['playlist'][$youtube->getUser()->data()->id.' - '.$key]) echo 'checked="checked"'; 
+ ?>  
+     
+
+</form>
+
+<?php
+//  if (isset($_POST['youtube_playlists'])){       
+//   //print '<h1>HELLO!</h1>';
+// if (is_array($_POST['playlist'])) {
+//   foreach ($_POST['playlist'] as $key => $value) {
+//     print 'this is key '.$key . ' and this is value '.$value.'<br />';
+//   }
+//   print_r($_POST['playlist']);
+// }
+// }
+
+if(Input::get('youtube_playlists')){
+  print_r(Input::get('playlist'));
+}
+
+?>
+</div>      
+
+
+
+           
 </div>
-
-
-
-
 
 <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscript.js"></script>
